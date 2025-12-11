ba629a3a15fdf216c010205a539182d166f18e94
SJKZ1-2565 <seefuture13@gmail.com>
2025-12-11 16:27:44 +0700

    chore: use varible instead

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+
 int main()
 {
     int workingHours;
+    int limitWokingHours = 6;
     int salary;
 
     printf("Enter your working time : ");
     scanf("%d",&workingHours);
     printf("Your working hour is %d" " hours\n",workingHours);
 
-    if(workingHours > 6) {
-        int otHours = workingHours - 6;
+    if(workingHours > limitWokingHours) {
+        int otHours = workingHours - limitWokingHours;
         salary = (workingHours-otHours) * (100) + (otHours * 200);
         printf("Your overtime working hours is %d\n",otHours);
         printf("Your salary is %d\n",salary);
